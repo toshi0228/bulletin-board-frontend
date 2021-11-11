f4e79d9f0f8e17c1e92f518709bc035b2c6713c7
toshi0228 <etoshi0228@gmail.com>
2021-11-11 09:05:23 +0900

    jwtからuidを取得できるようにする

--- src/helper/storage.ts
+++ src/helper/storage.ts
@@ -1,8 +1,15 @@
+import jwt_decode from "jwt-decode";
+
 const storage = window.localStorage;
 
 class Storage {
   private readonly tokenName: string = "token";
   private readonly userName: string = "userName";
+
+  get uid(): string {
+    const token = storage.getItem(this.tokenName) as string;
+    return jwt_decode<{ userId: string }>(token).userId; // jwtのtokenからuidを取得
+  }
 
   get token(): string {
     return storage.getItem(this.tokenName) as string;
